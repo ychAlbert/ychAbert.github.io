d0304d2c889130bf87dc066e2ab83d4bd3911022
叶晨昊 <126464945+ychAlbert@users.noreply.github.com>
2023-05-19 14:15:36 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+let typed = new Typed('#typed-text', {
+    strings: ['几乎不可避免的是，晚霞是世界的闭眼吧','亲爱的，我想对你说，', '我喜欢你，', '我爱你，', '我希望和你在一起，', '直到永远。'],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true,
+    showCursor: true,
+    cursorChar: '|',
+    smartBackspace: true,
+    backDelay: 1000,
+    startDelay: 1000
+  });
